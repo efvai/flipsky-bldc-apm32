395288bc39167c7e5df8ba111cc30116418e232e
efvai <jkcvkdslf@gmail.com>
2024-08-16 19:42:00 +0400

    init

---
+++ src/main.h
@@ -0,0 +1,16 @@
+#include "stm32f4xx_hal.h"
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
